7c7bba94a82d99f5c96be9d3cd7f1e37039afc9b
weng zhang <weng.zhang@icloud.com>
2025-12-07 21:13:48 +0000

    ex00 and ex01

---
+++ assignment01/main.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("zweng");
+MODULE_DESCRIPTION("Simple Hello World Module");
+
+static int __init hello_init(void)
+{
+    pr_info("Hello world!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    pr_info("Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
